775cc68b2366f216997bdcf3f014e7faa5613129
mukeshpa2 <mukeshpa4@yahoo.com>
2020-06-18 13:05:29 +0530

    Updates

---
+++ precache-manifest.cc5fda424c826461d596b6a4a9e0e0ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97d66691a73886f6c80c",
+    "url": "/CourseApp/static/css/main.24337ea9.chunk.css"
+  },
+  {
+    "revision": "97d66691a73886f6c80c",
+    "url": "/CourseApp/static/js/main.05cc0689.chunk.js"
+  },
+  {
+    "revision": "5928aa807337e97b8bd3",
+    "url": "/CourseApp/static/js/runtime~main.1736e2ae.js"
+  },
+  {
+    "revision": "389295ca42853ecd3ca6",
+    "url": "/CourseApp/static/js/2.45a63e45.chunk.js"
+  },
+  {
+    "revision": "3fa39178a9917357727a6d3a5ea1f025",
+    "url": "/CourseApp/index.html"
+  }
+];
